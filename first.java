fa694bec01c36f05e24429d699743fbf59fd31c2
Bharath Mohan Sivaselvam <BharathMohan_Sivaselvam@Trimble.com>
2021-03-10 20:12:49 +0530

    added first program

---
+++ first.java
@@ -0,0 +1,11 @@
+
+class first
+{
+    public static void main(String[] args)
+     {
+             for(int i=0;i<=10;i++)
+             {
+                System.out.println("Ain't no sunshine when She's gone" +i);
+             }
+}
+}
